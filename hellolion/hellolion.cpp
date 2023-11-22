c98fbf38b4b4e51a6c91044a394e92dd5213b5a5
Mauricio Inguanzo <inguansoft@gmail.com>
2023-11-22 01:11:26 -0800

    updates

---
+++ hellolion/hellolion.cpp
@@ -0,0 +1,14 @@
+//
+// Created by inguanzo on 11/21/23.
+//
+
+
+#include "hellolion.h"
+
+int main() {
+    int c = 100;
+    while (c--) {
+
+    }
+   return 0;
+}
---
+++ hellolion/hellolion.h
@@ -0,0 +1,8 @@
+//
+// Created by inguanzo on 11/21/23.
+//
+
+#ifndef HELLOLION_HELLOLION_H
+#define HELLOLION_HELLOLION_H
+
+#endif //HELLOLION_HELLOLION_H
