8aff4166dae853b17db5e56996725beb4816cd2c
Mauricio Inguanzo <inguansoft@gmail.com>
2023-12-22 13:05:17 -0800

    adding python

---
+++ prime/prime.py
@@ -0,0 +1,34 @@
+
+def process_prime_list(number):
+    results = []
+    i = number
+    while i > 1:
+        j=i - 1
+        while j > 1:
+            if i%j == 0:
+                break
+            j-=1
+        if j == 1:
+            results.append(i)
+        i-=1
+        
+    return results
+
+def valid_input(number):
+    return number > 0
+
+input_number = 1
+while (input_number != 0):
+    user_input = input("Prime numbers from 2 to N (0 to exit) N=")
+    if not user_input.isnumeric():
+        print("Expected numeric input")
+        continue
+    input_number = int(user_input)
+    if valid_input(input_number):
+        prime_list = process_prime_list(input_number)
+        print (f"Calculated {prime_list}")
+    else:
+        if(input_number == 0):
+            print("Bye")
+        else:
+            print("Expected input > 0, please try again.")
