131d621eb59baa5d8087ffc8a305a9b175ff756e
Mauricio Inguanzo <inguansoft@gmail.com>
2024-01-30 23:21:08 -0800

    bitwise rolling

--- bitwise_data/bitwise_data.c
+++ bitwise_data/bitwise_data.c
@@ -1,42 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
+int main(void)
+{
+    // 0x01
+    unsigned int basket = 0x00, i = 0x12345678, x, k = 0x01, shift_counter = 31, direction = -2;
+    printf("Trailing back and forth %08x\n", i);
+    while (1)
+    {
+        if (direction != 1)
+        {
+            x = basket << 1;
+            k = 0x01;
+        }
+        else
+        {
+            x = basket >> 1;
+            k = 0x80000000;
+        }
+        if (0x00000001 & (i >> shift_counter))
+        {
+            x = x | k;
+        }
+        basket = x;
 
-/**
- * 16 bits
- * @param number
- * @return
- */
-int print_pattern(uint number) {
-    int i, j, k;
-    if (number > 0 && number < 41) {
-        for (i=number; i>0; i--){ // Iterations
-            for (j=i; j>0; j--) { //Number
-                for (k=i; k>0; k--) { //Iterate
-                    printf("%d ", j);
-                }
+        if (shift_counter == 0 || shift_counter == 31)
+        {
+            if (direction == -2)
+            {
+                direction = -1;
             }
-            printf("$");
+            else
+            {
+                printf("Change direction to %c combo set %08x\n",
+                       (direction == 1) ? '>' : '<',
+                       basket);
+                direction *= -1;
+            }
         }
-        printf("\n");
-        return 0;
-    } else {
-        printf("Number has to be between 1 and 40\n");
-        return 1;
+        printf("0x%08x\n", basket);
+        shift_counter += direction;
+        sleep(1);
     }
-}
-
-int main(void) {
-
-    int i=0;
-    for (i=0;i<5;i++) {
-        printf("Getting pattern for %02x\n",i);
-        if (print_pattern(i) == 0) {
-            printf("Successfully loaded %02x!\n",i);
-        } else {
-            printf("EXIT with failure using %02x!\n",i);
-        }
-    }
-    return EXIT_SUCCESS;
-
 }
