d7ba256a0a976b7f5a3e5ea5f905e93fa1d3f260
Mauricio Inguanzo <inguansoft@gmail.com>
2024-01-31 21:44:12 -0800

    set bits

--- bitwise_data/play.cpp
+++ bitwise_data/play.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 int main()
 {
-    unsigned int x = 0x80000000;
-
+    unsigned int x = 0x80000000,
+                 main = 0xa0bb1234, fragment = 0x95;
+    // Objective insert in the 0x000XX000
     for (int i = 0; i < 32; i++)
     {
         cout << bitset<32>(x >> i) << endl;
@@ -18,5 +19,8 @@ int main()
     {
         cout << bitset<32>(x << i) << endl;
     }
+
+    printf("%08x --masked--> %08x\n", main,
+           (main | 0x00FF0000) & ((fragment << 16) | 0xFF00FFFF));
     return 0;
 }
