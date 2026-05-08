9ee1fe011e7f4634f5d488e2dfc0df7558a99b57
inguanzo <inguansoft@gmail.com>
2026-05-07 22:50:15 -0700

    workout

--- bitwise_data/reg_bit.c
+++ bitwise_data/reg_bit.c
@@ -7,16 +7,16 @@ volatile uint32_t * const GPIO_OUT =
     (volatile uint32_t *)REG_ADDR;
 
 /* write your macros here */
-#define BIT_SET(reg, n)   (UINT32_C(1) << n | reg )
-#define BIT_CLR(reg, n)   (~(UINT32_C(1) << n) & reg)
-#define BIT_TOG(reg, n)   ((UINT32_C(1) << n) ^ reg )
+#define BIT_SET(reg, n)   (reg |= (UINT32_C(1) << (n) ))
+#define BIT_CLR(reg, n)   (reg &= (~(UINT32_C(1) << (n))))
+#define BIT_TOG(reg, n)   (reg ^= (UINT32_C(1) << (n)))
 
 //  uint32_t sample = 151;
 //  EXPECT_EQ(BIT_SET(22, 9), 55);
 
 //int main(int argc, char **argv) {
 int main(void) {
-  volatile uint32_t *sample;
+  volatile uint32_t * sample;
   
   uint32_t mock_reg = 0x00000022;
   sample = &mock_reg;
@@ -24,8 +24,8 @@ int main(void) {
   printf("%s -> got 0x%08x\n", 
     (BIT_SET(*sample, 8) == 0x00000122)? "PASS" : "FAIL", BIT_SET(*sample, 8));
   printf("%s ->  0x%08x == 0x00000020\n", 
-    (BIT_CLR(*sample, 1) == 0x00000020)? "PASS" : "FAIL", BIT_CLR(*sample, 1));
+    (BIT_CLR(*sample, 1) == 0x00000120)? "PASS" : "FAIL", BIT_CLR(*sample, 1));
   printf("%s -> got 0x%08x\n", 
-    (BIT_TOG(*sample, 8) == 0x00000122)? "PASS" : "FAIL", BIT_TOG(*sample, 8));
+    (BIT_TOG(*sample, 8) == 0x00000020)? "PASS" : "FAIL", BIT_TOG(*sample, 8));
   return 0;
 }
